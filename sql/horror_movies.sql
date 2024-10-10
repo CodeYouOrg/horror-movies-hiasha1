f0e63f07262b328a98a3f3ad57437484f0cc3511
hiasha1 <95436084+hiasha1@users.noreply.github.com>
2024-10-10 19:35:58 -0400

    code added

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,2 +1,7 @@
-SELECT id,name,imdb_rating from movies
-where name in ('The Shining','Gremlins','The Amityville Horror')order by imdb_rating DESC;
+SELECT id as Movie_ID,
+name as Movie_Title,
+imdb_rating as Rating
+ from movies
+where year <= 1985 and genre = 'horror'
+order by imdb_rating DESC
+LIMIT 3;
